a0c25320f31f97015de4c8fd8d7d23420d64c2e5
Mustafa Tekin <mustafa21tekin@gmail.com>
2024-10-13 16:06:36 -0500

    Created Sample Sort file and folder

---
+++ Sample_Sort/sample_sort.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * FILE: mpi_mm.cpp
+ * DESCRIPTION:
+ *   MPI implementation of Sample Sort with Caliper instrumentation.
+ * AUTHOR:
+ *   Mustafa Tekin
+ * LAST REVISED:
+ *   ...
+ ******************************************************************************/
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
